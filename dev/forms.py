74cc4aec41bf9be18f8a3ec777405e00695d7b4c
Miras <jumajanovmiras@gmail.com>
2024-07-04 03:37:22 +0600

    Uploading major files

---
+++ dev/forms.py
@@ -0,0 +1,55 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(
+        label='',
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email Address'})
+    )
+    first_name = forms.CharField(
+        label='',
+        max_length=100,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First name'})
+    )
+    last_name = forms.CharField(
+        label='',
+        max_length=100,
+        required=False,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last name'})
+    )
+    username = forms.CharField(
+        label='',
+        max_length=150,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}),
+        help_text='<span class="form-text text-muted"><small>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small></span>'
+    )
+    password1 = forms.CharField(
+        label='',
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}),
+        help_text='<ul class="form-text text-muted small"><li>Your password can\'t be too similar to your other personal information.</li><li>Your password must contain at least 8 characters.</li><li>Your password can\'t be a commonly used password.</li><li>Your password can\'t be entirely numeric.</li></ul>'
+    )
+    password2 = forms.CharField(
+        label='',
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm Password'}),
+        help_text='<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+        for field_name in self.fields:
+            field = self.fields.get(field_name)
+            if field:
+                field.widget.attrs.update({'class': 'form-control'})
+                field.label = ''
+
+
+        self.fields['username'].widget.attrs['placeholder'] = 'Username'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
---
+++ dev/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import SignUpForm
+
+
+# Create your views here.
+def main(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password1']
+        user = authenticate(username=username, password=password)
+        if user:
+            login(request, user)
+            messages.success(request, 'You have been logged in!')
+            return redirect('home')
+        else:
+            messages.success(request, 'There is an error, please try again')
+            return redirect('home')
+        
+    return render(request, 'dev/home.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'You have been logged out')
+    return redirect('home')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'You have succsessfully Registered')
+            return redirect('home')
+            
+    else:
+        form = SignUpForm()
+        return render(request, 'dev/register.html', {'form': form})
+    return render(request, 'dev/register.html', {'form': form})
+    
